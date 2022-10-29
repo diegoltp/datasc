41598c7b4e54dfa0797b9d6677a426f541b6d10b
diego <diego.lizana.tapia@gmail.com>
2022-10-28 23:06:51 -0300

    observacion f-string

--- g56/s1/c2/word_count.py
+++ g56/s1/c2/word_count.py
@@ -2,7 +2,7 @@ with open("lorem_ipsum.txt","r") as file:
     texto = file.read()
 #parte a 
 parte_a = set(texto)
-print("El número de caracteres distintos es: " + str(len(parte_a)))
+print(f"El número de caracteres distintos es: {str(len(parte_a))}")
 #parte b
 parte_b = set(texto.split(" "))
-print("El número de elementos distintos es: " + str(len(parte_b)))
+print(f"El número de elementos distintos es: {str(len(parte_b))}")
