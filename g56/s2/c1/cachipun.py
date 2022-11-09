aa3ed0357f9362981ae5a99eb173ad1831e91aa6
diego <diego.lizana.tapia@gmail.com>
2022-11-08 21:35:32 -0300

    sem2clase1 tarea1

--- g56/s2/c1/cachipun.py
+++ g56/s2/c1/cachipun.py
@@ -4,28 +4,38 @@ from sys import argv
 user_choice = argv[1]
 sist_choice = choice(["Piedra", "Papel", "Tijera"])
 versus = user_choice+sist_choice
-if versus == "PiedraPiedra":
-    resultado = "Empate"
-elif versus == "PiedraPapel":
-    resultado = "Gana la I.A."
-elif versus == "PiedraTijera":
-    resultado = "Gana el usuario"
-elif versus == "PapelPiedra":
-    resultado = "Gana el usuario"
-elif versus == "PapelPapel":
-    resultado = "Empate"
-elif versus == "PapelTijera":
-    resultado = "Gana la I.A."
-elif versus == "TijeraPiedra":
-    resultado = "Gana la I.A."
-elif versus == "TijeraPapel":
-    resultado = "Gana el usuario"
-elif versus == "TijeraTijera":
-    resultado = "Empate"
-            
-print(f"""----------------------------------------
+#Verificar variable
+if user_choice == "Piedra" or user_choice == "Papel" or user_choice == "Tijera":
+    #Condiciones versus
+    if versus == "PiedraPiedra":
+        resultado = "Empate"
+    elif versus == "PiedraPapel":
+        resultado = "Gana la I.A."
+    elif versus == "PiedraTijera":
+        resultado = "Gana el usuario"
+    elif versus == "PapelPiedra":
+        resultado = "Gana el usuario"
+    elif versus == "PapelPapel":
+        resultado = "Empate"
+    elif versus == "PapelTijera":
+        resultado = "Gana la I.A."
+    elif versus == "TijeraPiedra":
+        resultado = "Gana la I.A."
+    elif versus == "TijeraPapel":
+        resultado = "Gana el usuario"
+    elif versus == "TijeraTijera":
+        resultado = "Empate"
+    #resultado
+    print(f"""----------------------------------------
 Tu jugaste {user_choice}
 Nuestra I.A. Jugo {sist_choice}
 {resultado}
 ----------------------------------------
+""")
+else:
+    print(f"""Argumento inválido!
+Opción debe ser igua la lista:
+> Piedra
+> Papel
+> Tijera
 """)
