f75d139c2c8751d00f7c42270fd4e9d4ea5de93c
diego <diego.lizana.tapia@gmail.com>
2022-11-09 21:10:08 -0300

    sem2clase1 tarea1.2

--- g56/s2/c1/cachipun.py
+++ g56/s2/c1/cachipun.py
@@ -2,40 +2,37 @@ from random import choice
 from sys import argv
 #Declarar variables
 user_choice = argv[1]
-sist_choice = choice(["Piedra", "Papel", "Tijera"])
-versus = user_choice+sist_choice
-#Verificar variable
-if user_choice == "Piedra" or user_choice == "Papel" or user_choice == "Tijera":
-    #Condiciones versus
-    if versus == "PiedraPiedra":
-        resultado = "Empate"
-    elif versus == "PiedraPapel":
-        resultado = "Gana la I.A."
-    elif versus == "PiedraTijera":
-        resultado = "Gana el usuario"
-    elif versus == "PapelPiedra":
-        resultado = "Gana el usuario"
-    elif versus == "PapelPapel":
-        resultado = "Empate"
-    elif versus == "PapelTijera":
-        resultado = "Gana la I.A."
-    elif versus == "TijeraPiedra":
-        resultado = "Gana la I.A."
-    elif versus == "TijeraPapel":
-        resultado = "Gana el usuario"
-    elif versus == "TijeraTijera":
-        resultado = "Empate"
-    #resultado
-    print(f"""----------------------------------------
+error = True
+#Ciclo contenedor del programa
+while error:
+    if user_choice == "Piedra" or user_choice == "Papel" or user_choice == "Tijera":
+        sist_choice = choice(["Piedra", "Papel", "Tijera"])
+        versus = user_choice+sist_choice
+        opciones = {"PiedraPiedra":"Empate",
+                    "PiedraPapel":"Gana la I.A.",
+                    "PiedraTijera":"Gana el usuario",
+                    "PapelPiedra":"Gana el usuario",
+                    "PapelPapel":"Empate",
+                    "PapelTijera":"Gana la I.A.",
+                    "TijeraPiedra":"Gana la I.A.",
+                    "TijeraPapel":"Gana el usuario",
+                    "TijeraTijera":"Empate",
+                    }
+        print(f"""----------------------------------------
 Tu jugaste {user_choice}
 Nuestra I.A. Jugo {sist_choice}
-{resultado}
+{opciones[versus]}
 ----------------------------------------
-""")
-else:
-    print(f"""Argumento inválido!
-Opción debe ser igua la lista:
-> Piedra
-> Papel
-> Tijera
-""")
+        """)
+        terminar = input("Para terminar programa escriba SI\n> ")
+        if terminar == "SI":
+                error = False
+        else:
+            print("Reinicio automatico activado... ")
+            user_choice = input("Ingrese su nueva opccion:\n> ")
+    elif user_choice != "Piedra" or user_choice != "Papel" or user_choice != "Tijera":
+        print("Argumento inválido: Debe ser Piedra, Papel o Tijera.")
+        user_choice = input("Ingrese nuevamente la opccion:\n> ")
+#fin del ciclo
+
+print("Adios")
